7a1b22228b73b1eb69ecb76c1e73da8e568eaf71
GokhanDuzel <gduze020@uottawa.ca>
2024-04-26 17:20:08 -0400

    new chatbot

--- Chatbot with Open Source LLMs using Python and Hugging Face/chatbot.py
+++ Chatbot with Open Source LLMs using Python and Hugging Face/chatbot.py
@@ -9,22 +9,19 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 # Keeping track of conversation history
 conversation_history = []
 
-# Encoding the conversation history
-history_string = "\n".join(conversation_history)
-
-# Fetch prompt from user
-input_text ="hello, how are you doing?"
-
-# Tokenization of user prompt and chat history
-inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
-print(inputs)
-
-tokenizer.pretrained_vocab_files_map
-
-# Generate output from the model
-outputs = model.generate(**inputs)
-print(outputs)
-
-# Decode output
-response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-print(response)
+while True:
+    # Create conversation history string
+    history_string = "\n".join(conversation_history)
+    # Get the input data from the user
+    input_text = input("> ")
+    # Tokenize the input text and history
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+    # Generate the response from the model
+    outputs = model.generate(**inputs)
+    # Decode the response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    
+    print(response)
+    # Add interaction to conversation history
+    conversation_history.append(input_text)
+    conversation_history.append(response)
